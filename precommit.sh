9107cb1f0477891c7d9894c532d9ae459173d818
Mikhail Sadiakhmatov <fortis3000@gmail.com>
2024-05-07 18:17:55 +0200

    fix: precommit improved

--- precommit.sh
+++ precommit.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-export POETRY_PATH="poetry"
-
 echo "List of changes fields comparing to master branch"
 #git diff --stat master
 export CHANGED_FILES=$(git diff --name-only --diff-filter=d master -- '***.py')
@@ -11,16 +9,8 @@ if [ -z "$CHANGED_FILES" ]; then
 fi
 
 echo
-echo "Running isort import sorting"
-${POETRY_PATH} run python -m isort -v --settings-file ./pyproject.toml $CHANGED_FILES
-
-echo
-echo "Running black formatting"
-${POETRY_PATH} run python -m black -v $CHANGED_FILES
-
-echo
-echo "Running pylint linting"
-${POETRY_PATH} run python -m pylint $CHANGED_FILES
+ruff format $CHANGED_FILES --config pyproject.toml
+ruff check $CHANGED_FILES --fix --config pyproject.toml
 
 echo
 echo "Finished"
