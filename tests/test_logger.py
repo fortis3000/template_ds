86579cad3b4708e4a242062d3e8b249c5ca1789d
Mikhail Sadiakhmatov <38611558+fortis3000@users.noreply.github.com>
2025-09-16 18:34:09 +0000

    chore: bugfix

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,7 +1,10 @@
-import pytest
+import pytest  # noqa: F401
 from src.utils import logger
 
-def test_logger_basic_usage():
+
+def test_logger_basic_usage(caplog):
     log = logger.get_logger("test")
+    with caplog.at_level("INFO"):
+        log.info("Logger works!")
     assert log.name == "test"
-    log.info("Logger works!")
+    assert any("Logger works!" in message for message in caplog.messages)
